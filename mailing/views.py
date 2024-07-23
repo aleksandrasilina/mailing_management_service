5ffb5455e83b4b9059ab1addce26428fb0575647
Александра <aleksandrasilina@gmail.com>
2024-07-23 15:24:26 +0300

    add ClientListView to views.py

--- mailing/views.py
+++ mailing/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from mailing.models import Client
+
+
+class ClientListView(ListView):
+    model = Client
