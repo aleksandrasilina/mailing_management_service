12a2b03bb3810af8da36e7f197d90499ab130ffa
Александра <aleksandrasilina@gmail.com>
2024-07-23 15:24:49 +0300

    change urls.py

--- mailing/urls.py
+++ mailing/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
 from mailing.apps import MailingConfig
+from mailing.views import ClientListView
 
 app_name = MailingConfig.name
 
 urlpatterns = [
-
+    path('', ClientListView.as_view(), name='client_list'),
 ]
