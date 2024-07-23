e89d0b7ca0b43dc046ea0d58278b48cba9fd7c2c
Александра <aleksandrasilina@gmail.com>
2024-07-23 17:15:27 +0300

    add ClientDetailView and client_detail.html

--- mailing/urls.py
+++ mailing/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from mailing.apps import MailingConfig
-from mailing.views import ClientListView
+from mailing.views import ClientListView, ClientDetailView
 
 app_name = MailingConfig.name
 
 urlpatterns = [
     path('', ClientListView.as_view(), name='client_list'),
+    path('client_detail/<int:pk>/', ClientDetailView.as_view(), name='client_detail'),
 ]
--- mailing/views.py
+++ mailing/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from mailing.models import Client
 
 
 class ClientListView(ListView):
+    model = Client
+
+
+class ClientDetailView(DetailView):
     model = Client
