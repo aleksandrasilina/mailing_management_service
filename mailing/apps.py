fe3f20d9954106cd544dd8238fceb3e6392a3a79
Александра <aleksandrasilina@gmail.com>
2024-08-30 13:39:00 +0300

    change mailing/ apps.py add start_mailing function in ready

--- mailing/apps.py
+++ mailing/apps.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 from django.apps import AppConfig
 
 
@@ -5,3 +7,7 @@ class MailingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'mailing'
     verbose_name = 'Рассылки'
+
+    # def ready(self):
+    #     from mailing.services import start_mailing
+    #     start_mailing()
