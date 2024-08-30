e9c0f083c7e14f1bf0677c927d13d9f2d2b08113
Александра <aleksandrasilina@gmail.com>
2024-08-30 13:36:08 +0300

    change mailing/ admin.py

--- mailing/admin.py
+++ mailing/admin.py
@@ -5,28 +5,75 @@ from mailing.models import Client, Message, Mailing, MailingLog
 
 @admin.register(Client)
 class ClientAdmin(admin.ModelAdmin):
-    list_display = ('id', 'first_name', 'last_name', 'email',)
-    search_fields = ('first_name', 'last_name', 'email',)
-    list_display_links = ('id', 'first_name',)
+    list_display = (
+        "id",
+        "first_name",
+        "last_name",
+        "email",
+    )
+    search_fields = (
+        "first_name",
+        "last_name",
+        "email",
+    )
+    list_display_links = (
+        "id",
+        "first_name",
+    )
 
 
 @admin.register(Message)
 class MessageAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title',)
-    search_fields = ('title', 'body',)
-    list_display_links = ('id', 'title',)
+    list_display = (
+        "id",
+        "title",
+    )
+    search_fields = (
+        "title",
+        "body",
+    )
+    list_display_links = (
+        "id",
+        "title",
+    )
 
 
 @admin.register(Mailing)
 class MailingAdmin(admin.ModelAdmin):
-    list_display = ('id', 'message', 'created_at', 'periodicity', 'mailing_status', 'message',)
-    list_filter = ('periodicity', 'mailing_status', 'clients', 'message',)
-    list_display_links = ('id', 'message',)
+    list_display = (
+        "id",
+        "message",
+        "first_send_time",
+        "periodicity",
+        "mailing_status",
+    )
+    list_filter = (
+        "periodicity",
+        "mailing_status",
+        "clients",
+        "message",
+    )
+    list_display_links = (
+        "id",
+        "message",
+    )
 
 
 @admin.register(MailingLog)
 class MailingLogAdmin(admin.ModelAdmin):
-    list_display = ('id', 'sent_at', 'is_success', 'mailing',)
-    list_filter = ('is_success', 'is_success', 'mailing',)
-    search_fields = ('server_response',)
-    list_display_links = ('id', 'sent_at',)
+    list_display = (
+        "id",
+        "sent_at",
+        "is_success",
+        "mailing",
+    )
+    list_filter = (
+        "is_success",
+        "is_success",
+        "mailing",
+    )
+    search_fields = ("server_response",)
+    list_display_links = (
+        "id",
+        "sent_at",
+    )
