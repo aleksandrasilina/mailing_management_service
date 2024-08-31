dbe66274fa00c1d159bbf1b6ae4c10b35eb391c2
Александра <aleksandrasilina@gmail.com>
2024-08-31 15:19:20 +0300

    change users/urls.py add email_verification, UserPasswordResetView, PasswordResetDoneView

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,8 @@
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordResetDoneView
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import RegisterView, ProfileView
+from users.views import RegisterView, ProfileView, email_verification, UserPasswordResetView
 
 app_name = UsersConfig.name
 
@@ -10,8 +10,9 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
-    # path('email-confirm/<str:token>', email_verification, name='email-confirm'),
     path('profile/', ProfileView.as_view(), name='profile'),
-    # path('password-reset/', UserPasswordResetView.as_view(), name='password-reset'),
-    # path('password-reset-done/', PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password-reset-done')
+    path('email-confirm/<str:token>', email_verification, name='email-confirm'),
+    path('password-reset/', UserPasswordResetView.as_view(), name='password-reset'),
+    path('password-reset-done/', PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
+         name='password-reset-done')
 ]
