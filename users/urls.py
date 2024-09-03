39015355e3c3e93ff5c28f3d48c97eee86a7ea0b
Александра <aleksandrasilina@gmail.com>
2024-09-03 13:54:59 +0300

    change users\ urls.py add UserLoginView

--- users/urls.py
+++ users/urls.py
@@ -1,18 +1,22 @@
-from django.contrib.auth.views import LoginView, LogoutView, PasswordResetDoneView
+from django.contrib.auth.views import LogoutView, PasswordResetDoneView
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import RegisterView, ProfileView, email_verification, UserPasswordResetView
+from users.views import RegisterView, ProfileView, email_verification, UserPasswordResetView, UserListView, \
+    toggle_activity, UserLoginView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
-    path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('login/', UserLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
     path('profile/', ProfileView.as_view(), name='profile'),
     path('email-confirm/<str:token>', email_verification, name='email-confirm'),
     path('password-reset/', UserPasswordResetView.as_view(), name='password-reset'),
     path('password-reset-done/', PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
-         name='password-reset-done')
+         name='password-reset-done'),
+    path('user_list/', UserListView.as_view(), name='user_list'),
+    path('activity/<int:pk>/', toggle_activity, name='toggle_activity'),
+
 ]
