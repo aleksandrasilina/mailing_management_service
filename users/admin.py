ac55635bdab54684e6cf426a687e80f4142600d4
Александра <aleksandrasilina@gmail.com>
2024-09-03 13:54:08 +0300

    change users\ admin.py

--- users/admin.py
+++ users/admin.py
@@ -2,4 +2,21 @@ from django.contrib import admin
 
 from users.models import User
 
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "email",
+        "first_name",
+        "last_name",
+        "get_groups",
+    )
+    list_display_links = (
+        "id",
+        "email",
+    )
+
+    @admin.display(description='группы')
+    def get_groups(self, obj):
+        return [group for group in obj.groups.all()]
