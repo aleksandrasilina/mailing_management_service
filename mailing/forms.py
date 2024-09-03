36ac9fccb3ede86a9712cce2dfe6938e48c46171
Александра <aleksandrasilina@gmail.com>
2024-09-03 13:51:18 +0300

    change mailing\ forms.py add MailingModeratorForm add self.fields["message"].queryset, self.fields["clients"].queryset

--- mailing/forms.py
+++ mailing/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from mailing.models import Mailing
+from mailing.models import Mailing, Message, Client
 
 
 class DateTimeInput(forms.DateTimeInput):
@@ -14,9 +14,24 @@ class DateTimeInput(forms.DateTimeInput):
 class MailingForm(forms.ModelForm):
     class Meta:
         model = Mailing
-        exclude = ('mailing_status',)
+        exclude = ('mailing_status', 'owner')
 
     def __init__(self, *args, **kwargs):
+        user = kwargs.pop("user")
         super().__init__(*args, **kwargs)
+
         self.fields["first_send_time"].widget = DateTimeInput()
         self.fields["last_send_time"].widget = DateTimeInput()
+
+        self.fields["message"].queryset = Message.objects.filter(owner=user)
+        self.fields["clients"].queryset = Client.objects.filter(owner=user)
+
+
+class MailingModeratorForm(forms.ModelForm):
+    class Meta:
+        model = Mailing
+        fields = ('mailing_status',)
+
+    def __init__(self, *args, **kwargs):
+        user = kwargs.pop("user")
+        super().__init__(*args, **kwargs)
